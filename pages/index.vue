5b6df4864c2219324c51e9c88dbf486d8fd2c7d5
Shinpei Noda <noda@haw.co.jp>
2018-01-15 20:44:40 +0900

    :new: Add link to quanon.github.io

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,9 @@
     <div class="ui four doubling stackable cards">
       <AddCounter/>
     </div>
+    <div class="ui footer">
+      <a href="https://quanon.github.io/">quanon.github.io</a>
+    </div>
   </div>
 </template>
 
@@ -35,5 +38,15 @@ export default {
 
 .extra.content {
   padding: 0 !important;
+}
+
+.footer {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 14px;
+
+  a, a:visited, a:hover, a:active {
+    color: rgba(0, 0, 0, .6)
+  }
 }
 </style>
